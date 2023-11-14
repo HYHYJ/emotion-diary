40c2f4dd261a0a271700d116f7ee654ddb54c246
HYHYJ <jjangdaram@naver.com>
2023-11-14 22:15:03 +0900

    Page Moving : useNavigate = Link태그 안쓰고 넘어가게하기

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,6 +1,8 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
+  // 📘Page Moving : useNavigate = Link태그 안쓰고 넘어가게하기
+  const navigate = useNavigate();
   // ✏️useSearchParams :배열을 반환한다.
   const [searchParams, setsearchParams] = useSearchParams();
   //✏️get을 써서 전달받은 쿼리 스크링을 전달 받을 수 있다.
@@ -16,6 +18,22 @@ const Edit = () => {
       {/* ✏️setsearchParams로 주소값 바꾸기*/}
       <button onClick={() => setsearchParams({ who: "winterlood" })}>
         QS바꾸기
+      </button>
+      {/* 📘Page Moving 으로 홈페이지로 넘어간다. */}
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      {/* 📘뒤로가기 버튼 : 뒤로 한번가서 -1 */}
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
       </button>
     </div>
   );
