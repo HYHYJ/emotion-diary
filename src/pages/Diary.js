0a4e09e0faeebd394a696dee08330ab7ed52c9a4
HYHYJ <jjangdaram@naver.com>
2023-11-14 22:06:36 +0900

    1. Path Variable: useParams, 2. useSearchParams

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,4 +1,11 @@
+//라이브러리가 만든 사용자정의 커스텀 훅스
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
+  // ✏️패스 베리어블(id)을 꺼내쓸수있음
+  // ✏️페이지 번호가 1 이면 1을 꺼냄
+  const { id } = useParams();
+  console.log(id);
   return (
     <div>
       <h1>Diary</h1>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,22 @@
+import { useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  // ✏️useSearchParams :배열을 반환한다.
+  const [searchParams, setsearchParams] = useSearchParams();
+  //✏️get을 써서 전달받은 쿼리 스크링을 전달 받을 수 있다.
+  const id = searchParams.get("id");
+  console.log(id);
+
+  const mode = searchParams.get("mode");
+  console.log(mode);
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지 입니다.</p>
+      {/* ✏️setsearchParams로 주소값 바꾸기*/}
+      <button onClick={() => setsearchParams({ who: "winterlood" })}>
+        QS바꾸기
+      </button>
     </div>
   );
 };
